3b59133fc0b929b540b8cefca1d4e1364e897bb2
Kim <a01088570194@gmail.com>
2019-04-10 00:36:44 +0900

    Books Create/Index/New

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Book = require('../models/book');
 
 router.get('/', (req, res) => {
-    res.render('index');
+    let books;
+    try {
+        books = Book.find().sort({ createAt: 'desc' }).limit(10).exec();
+    } catch {
+        books = [];
+    }
+    res.render('index', { books: books });
 })
 
 module.exports = router;
